dc7f24ab3ea131701d1788e2fead8d6e57ea6965
Diogo Santos <diogo_2806@hotmail.com>
2026-09-21 20:06:06 -0300

    refactor(frontend): apontar App para a landing reorganizada (#31)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { LandingPage } from "./components/LandingPage";
+import { LandingPage } from "./app/publico/landing/LandingPage";
 
 export default function App() {
   return <LandingPage />;
